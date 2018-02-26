85ec47e07be3460ce751aa5c9a09a29efe17ece4
Gandalf1452 <36859168+Gandalf1452@users.noreply.github.com>
2018-02-26 13:18:09 -0500

    Create RPS_RH.py

---
+++ RPS_RH.py
@@ -0,0 +1,167 @@
+########How I did scoring is that I based it off of whether or not you won, then went off of the ratio of Player Wins to CPU Wins, making it a 1 to 10 scale#################
+########For tie scores, the new score is added next to the old score##############
+import random
+import time
+ranNum = 0
+end = False
+global playerWins
+global cpuWins
+global cpuChoicee
+global playAgain1
+global playAgain2
+global score
+global score_file
+global scores
+global topTen
+global fileName
+global highScores
+userName = input("What is your name? ")
+fileName = input("Name the file where scores will be held: ")
+
+######This function allows the player to view the top 10 scores if they wish#########
+def view_scores():
+    highScores = input("Would you like to view the high scores?(Y/N) ")
+    if highScores.startswith("y") or highScores.startswith("Y"):
+        words = []
+        score_file2 = open(fileName + ".txt","r")
+        for line in score_file2:
+            words += line.split()
+        words.sort()
+        words.reverse()
+        score_file2.close()
+        print("Top Scores")
+        print("-------------")
+        if len(words) == 11:
+            words.remove(words[10])
+            print(words)
+        else:
+            print(words)
+    elif highScores.startswith("n") or highScores.startswith("N"):
+        print("OK then, if that is your choice!")
+    else:
+        print("Invalid input, try again")
+        view_scores()      
+topTen = []
+cpuChoicee = "0"
+playerWins = 0
+cpuWins = 0
+
+######This function generates the CPU's choice of Rock, Paper, Scissors######
+def gen_num():
+    global cpuChoicee
+    global ranNum
+    ranNum = random.randint(1,3)
+    if ranNum == 1:
+        cpuChoicee = "Rock"
+    elif ranNum == 2:
+        cpuChoicee = "Paper"
+    elif ranNum == 3:
+        cpuChoicee = "Scissors"
+        
+#######This function gets the score based on the outcome of the game#######        
+def score_gen():
+    global score
+    if playerWins == 5 and cpuWins == 0:
+        score = 10
+    elif playerWins == 5 and cpuWins == 1:
+        score = 9
+    elif playerWins == 5 and cpuWins == 2:
+        score = 8
+    elif playerWins == 5 and cpuWins == 3:
+        score = 7
+    elif playerWins == 5 and cpuWins == 4:
+        score = 6
+    elif playerWins == 0 and cpuWins == 5:    
+        score = 1
+    elif playerWins == 1 and cpuWins == 5:
+        score = 2
+    elif playerWins == 2 and cpuWins == 5:
+        score = 3
+    elif playerWins == 3 and cpuWins == 5:
+        score = 4
+    elif playerWins == 4 and cpuWins == 5:
+        score = 5
+    else:
+        score = 0
+        
+#######This function writes the score out to the specified file########        
+def get_score():
+    s = " "
+    topTen = (str(score) + "," + userName)
+    s.join(topTen)
+    score_file = open(fileName + ".txt","a+")
+    score_file.write("\n" + topTen)
+    score_file.close()
+
+#######This is the while loop where the Rock Paper Scissors game runs####################        
+while end == False:
+    global score
+    global playerChoice
+    global cpuChoice
+    gen_num()
+    cpuChoice = ranNum
+    if playerWins < 5 and cpuWins < 5:
+        #time.sleep(1)
+        print("The CPU has chosen...")
+        #time.sleep(1)
+        playerChoice = input("Choose Rock, Paper, or Scissors: ")
+        if playerChoice == "Rock" or playerChoice == "rock" or playerChoice == "R" or playerChoice == "r" :
+            playerChoice = 1
+            if cpuChoice == 1:
+                print("It is a tie!" + " The CPU picked " + cpuChoicee + "\nPlayer Wins: " + str(playerWins) + " || " + "CPU Wins: " + str(cpuWins))
+            elif cpuChoice == 2:
+                cpuWins += 1
+                print("CPU wins the round!" + " The CPU picked " + cpuChoicee + "\nPlayer Wins: " + str(playerWins) + " || " + "CPU Wins: " + str(cpuWins))
+            elif cpuChoice == 3:
+                playerWins += 1
+                print(userName + " wins the round!" + " The CPU picked " + cpuChoicee + "\nPlayer Wins: " + str(playerWins) + " || " + "CPU Wins: " + str(cpuWins))
+        elif playerChoice == "Paper" or playerChoice == "paper" or playerChoice == "P" or playerChoice == "p":
+            playerChoice = 2
+            if cpuChoice == 1:
+                playerWins += 1
+                print(userName + " wins the round!" + " The CPU picked " + cpuChoicee + "\nPlayer Wins: " + str(playerWins) + " || " + "CPU Wins: " + str(cpuWins))
+            elif cpuChoice == 2:
+                print("It is a tie!" + " The CPU picked " + cpuChoicee + "\nPlayer Wins: " + str(playerWins) + " || " + "CPU Wins: " + str(cpuWins))
+            elif cpuChoice == 3:
+                cpuWins += 1
+                print("CPU wins the round!" + " The CPU picked " + cpuChoicee + "\nPlayer Wins: " + str(playerWins) + " || " + "CPU Wins: " + str(cpuWins))
+        elif playerChoice == "Scissors" or playerChoice == "scissors" or playerChoice == "S" or playerChoice == "s":
+            playerChoice = 3
+            if cpuChoice == 1:
+                cpuWins += 1
+                print("CPU wins the round!" + " The CPU picked " + cpuChoicee + "\nPlayer Wins: " + str(playerWins) + " || " + "CPU Wins: " + str(cpuWins))
+            elif cpuChoice == 2:
+                playerWins+= 1
+                print(userName + " wins the round!" + " The CPU picked " + cpuChoicee + "\nPlayer Wins: " + str(playerWins) + " || " + "CPU Wins: " + str(cpuWins))
+            elif cpuChoice == 3:
+                print("It is a tie!" + " The CPU picked " + cpuChoicee + "\nPlayer Wins: " + str(playerWins) + " || " + "CPU Wins: " + str(cpuWins))
+        else:
+            print("You didn't choose Rock, Paper or Scissors!")
+    elif playerWins == 5:
+        score_gen()
+        print(userName + " wins!!!")
+        print("Score: " + str(score))
+        get_score()
+        view_scores()
+        playAgain1 = input("Play Again?(y/n) ")
+        if playAgain1 == "y" or playAgain1 == "Y":
+            playerWins = 0
+            cpuWins = 0
+        elif playAgain1 == "n" or playAgain1 == "N":
+            break
+        else:
+            print("Invalid input, try again")
+    elif cpuWins == 5:
+        score_gen()
+        print("Aww... the CPU won...")
+        print("Score: " + str(score))
+        get_score()
+        view_scores()
+        playAgain2 = input("Play Again?(y/n) ")
+        if playAgain2 == "y" or playAgain2 == "Y":
+            playerWins = 0
+            cpuWins = 0
+        elif playAgain2 == "n" or playAgain2 == "N":
+            break
+        else:
+            print("Invalid input, try again")
